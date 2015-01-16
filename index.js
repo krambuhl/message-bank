19595679731d538a651dc195081cdf338befee79
Evan Krambuhl <thekrambuhl@gmail.com>
2015-01-15 22:32:50 -0800

    api outlione

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-module.exports = function() {
+module.exports = Bank;
 
+function Bank(opts) {
+  this.options = opts;
 
-};
+  if (opts.types) { this.types(opts.types); }
+  if (opts.setup) { this.setup(opts.setup); }
+}
+
+Bank.prototype = require('./lib/prototype.js');
