0cbe75d25a486606d1dba20ec7056f4594960ea1
Evan Krambuhl <thekrambuhl@gmail.com>
2015-01-19 01:50:58 -0800

    sub/unsub tests passing

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ module.exports = Bank;
 
 function Bank(opts) {
   this.options = opts = (opts || {});
+
   this._store = {};
   this._config = {};
+  this._subscriptions = [];
 
   if (opts.setup) { this.setup(opts.setup); }
 }
--- tests/subscribe.test.js
+++ tests/subscribe.test.js
@@ -13,6 +13,30 @@ test('bank.subscribe(type, func)', function (t) {
   bank.dispatch('TEST', { genre: 'metal' }, { duper: true });
 });
 
+test('bank.subscribe(type, func)', function (t) {
+  var bank = new MsgBank();
+  t.plan(4);
+
+  bank.dispatch('TEST', { genre: 'metal' }, { duper: true });
+  bank.subscribe('TEST', function(d, o) {
+    t.equal(d.genre, 'metal');
+    t.equal(o.duper, true);
+  });
+  bank.dispatch('TEST', { genre: 'metal' }, { duper: true });
+});
+
+test('bank.subscribe(type, func, { immediate })', function (t) {
+  var bank = new MsgBank();
+  t.plan(1);
+
+  bank.dispatch('TEST', { genre: 'metal' });
+  bank.subscribe('TEST', function(d, o) {
+    t.equal(d.genre, 'picklerock');
+  }, { immediate: false });
+
+  bank.dispatch('TEST', { genre: 'picklerock' });
+});
+
 test('id = bank.subscribe(type, func)', function (t) {
   var bank = new MsgBank();
   t.plan(1);
